be80b086957ff7884212841a4bd12f168141a260
raflihaidar <rafli.haidar30@gmail.com>
2026-05-16 23:57:29 +0700

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: "app",
-    script: "src/app.js",
+    script: "server/src/app.js",
     cwd: "/var/www/Monitoring-Jamur/server",
     env: {
       DATABASE_URL: "mysql://binatra:binatrA40_@localhost:3306/aeroponic_db"
